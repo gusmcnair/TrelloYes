70b05c567773148eaee013f94e15f0351b14bf0c
Angus McNair <amcnair234@gmail.com>
2019-11-16 19:32:50 -0600

    Third commit

--- src/list.test.js
+++ src/list.test.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './list';
-import STORE from './store.js';
 import renderer from 'react-test-renderer';
 
 describe ('List component', () => {
     it('renders without crashing', () => {
+        const testObject =  (  [{
+            id: '1',
+            header: 'First list',
+            cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+          }])
         const div = document.createElement('div');
-        ReactDOM.render(<List  cardInstance={props.card} />, div);
+        ReactDOM.render(<List  cardInstance={testObject} />, div);
         ReactDOM.unmountComponentAtNode(div);
-    });
-
+)};
     it('renders UI as expected', () => {
+        const testObject =  (  [{
+              id: '1',
+              header: 'First list',
+              cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+            }])
         const tree = renderer;
-            .create(<List cardInstance={props.card} />)
+            .create(<List cardInstance={testObject} />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
